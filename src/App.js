5f5226a0034823ae5db9ec0289b82429e274bfc6
skanda pandith <skandapandith@gmail.com>
2020-07-10 18:02:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React , { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 
 
-import './App.css';
 
-function App() {
+import './App.css';
+import { DISHES } from './shared/dishes';
+
+
+class App extends Component { 
+
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      dishes: DISHES
+
+    };
+  }
+render() {
   return (
     <div className="App">
       <Navbar dark color= "primary">
@@ -14,10 +27,12 @@ function App() {
           <NavbarBrand href= "/">Ristorante Con FUsion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={ this.state.dishes}/> 
      
     </div>
   );
 }
+}
+
 
 export default App;
